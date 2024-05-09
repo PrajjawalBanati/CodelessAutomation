4c61cb9cc81bc7c3940f835ed03b79764afcd62f
Prajjawal Banati <prajjawalbanati16@gmail.com>
2024-05-09 22:44:55 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 import TestcaseCreationPage from "./Pages/TestcaseCreationPage";
-import HomePage from "./Pages/HomePage";
+import Home from "./Pages/home";
+import Environment from "./Pages/environment"
 import TestcaseRunningPage from "./Pages/TestcaseRunningPage";
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/environment" element={<Environment />} />
           <Route path="/addtestcase" element={<TestcaseCreationPage />} />
           <Route path="/runtestcase" element={<TestcaseRunningPage/>} />
         </Routes>
